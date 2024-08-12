a7f41965693b367cd394370883a55e191666ce64
RobertKaribov <infobox.markets@gmail.com>
2024-08-12 15:25:50 +0300

    test1

--- src/main/java/ru/karibian/springboot/service/UserServicesImpl.java
+++ src/main/java/ru/karibian/springboot/service/UserServicesImpl.java
@@ -9,31 +9,32 @@ import ru.karibian.springboot.model.User;
 import java.util.List;
 
 @Service
+@Transactional
 public class UserServicesImpl implements UserServices {
 
-    private UserDao userDao;
+    private final UserDao userDao;
 
-    @Transactional(readOnly = true)
+    @Autowired
+    public UserServicesImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
     public List<User> findAllUsers() {
         return userDao.findAllUsers();
     }
 
-    @Transactional(readOnly = true)
     public User findUserById(int id) {
         return userDao.findUserById(id);
     }
 
-    @Transactional
     public void createUser(User user) {
         userDao.saveUser(user);
     }
 
-    @Transactional
     public void updateUser(int id, User updatedUser) {
         userDao.updateUser(id, updatedUser);
     }
 
-    @Transactional
     public void deleteUserById(int id) {
         userDao.deleteUser(id);
     }
